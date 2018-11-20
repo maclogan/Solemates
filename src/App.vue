d70be618ac780a62782af77c475719e7d7d16527
Mac Logan <p13mlogan@hotmail.com>
2018-11-19 22:23:13 -0600

    Created header component and minor styling

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,30 @@
   <template>
   <div class="app">
-    <div class="sidebar">
-      <div class="sidebar_content">      
-        <div class="top">
-        <a href="/"><div class="logo">sole<span>mates</span></div> </a>     
-        <img v-if="isLoggedIn" class="user-image" :src="user.image">
-        <button v-if="!isLoggedIn" @click='login()'>Login</button>
-      </div>
-      <button @click='logout()'>Logout</button>
-    </div></div>
-
-    <router-view class="main container"/>
+    <header-component></header-component>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { mapActions, mapState } from 'vuex';
+import HeaderComponent from './components/Header.vue';
+
 export default {
   name: 'app',
   computed: mapState('auth', ['user', 'isLoggedIn']),
   methods: mapActions('auth', ['login', 'logout']),
-  components: {},
+  components: { 
+    HeaderComponent
+  },
 };
 </script>
-
 <style>
 body {
-  width: 100vw;
-  height:100vh;
-  margin: 0;
-  padding: 0;
+  margin: 0px;
+  padding: 0px;
 }
-.app  {
-  display: flex;
-  justify-content: flex-start;
+header-component {
   width: 100%;
-  height: 100%;
+  height: 50px;
 }
-.sidebar {
-  width: 20em;
-  height: 100%;
-  margin-right: 1em;
-  background-color: #FFF;
-  border-radius: 0px 30px 30px 0px;
-  box-shadow: 4px 6px 8px rgba(0, 0, 0, 0.25);
-}
-.sidebar_content {
-  display: flex;
-  flex-direction: column;
-  align-items: space-around;
-  padding: 2em;
-}
-.top {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.logo {
-  font-size: 1.8em;
-  font-family: sans-serif;
-  font-weight: 100;
-}
-.logo span {
-  font-weight: 700;
-}
-.user-image {
-  height: 2em;
-  width: auto;
-  -webkit-clip-path: circle(50% at 50% 50%);
-  clip-path: circle(50% at 50% 50%);
-}
-
-button {
-  height: 2em;
-  width: 5em;
-  border-radius: 1em;
-}
-
-a {
-  color: inherit;
-  text-decoration: none;
-}
-
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="app">
+    <div class="header">
+      <div class="left">
+        <a href="/"><div class="logo">sole<span>mates</span></div> </a>     
+      </div>
+      <div class="center">
+          <input class="search" type="text">
+      </div>
+      <div class="right">
+        <button v-if="isLoggedIn">Post Item</button>
+        <img v-if="isLoggedIn" class="user-image" :src="user.image">
+        <button v-if="!isLoggedIn" @click='login()'>Login</button>
+        <!-- <button @click='logout()'>Logout</button> -->
+      </div>  
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapActions, mapState } from 'vuex';
+export default {
+  name: 'HeaderComponent',
+  computed: mapState('auth', ['user', 'isLoggedIn']),
+  methods: mapActions('auth', ['login', 'logout']),
+};
+</script>
+
+<style>
+.header {
+  background-color: #FFF;
+  box-shadow: 4px 6px 8px rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em 2em;
+  height: 2em;
+}
+
+.left {
+    display: flex;
+    align-content: center;
+}
+
+.logo {
+  font-size: 1.8em;
+  font-family: sans-serif;
+  font-weight: 100;
+}
+.logo span {
+  font-weight: 700;
+}
+
+.search {
+    font-size: 1em;
+    border-radius: 15px;
+    border: 1px solid rgb(160, 160, 160);
+    height: 2em;
+    padding: 0em 1em;
+}
+
+.right {
+    display: flex;
+    align-items: center;
+}
+.user-image {
+  height: 2em;
+  width: auto;
+  -webkit-clip-path: circle(50% at 50% 50%);
+  clip-path: circle(50% at 50% 50%);
+}
+button {
+    flex: 1 1 auto;
+    font-size: 1em;
+    margin: 2em;
+    display: inline-block;
+    text-align: center;
+    background-image: linear-gradient(to right, #43cea2 0%, #185a9d 100%);
+    border: none;
+    background-size: 200% auto;
+    border-radius: 15px;
+    height: 2em;
+    color: white;
+    padding: 0em 1em;
+    text-transform: uppercase;
+    box-shadow: 0 0 20px #eee;
+    transition: 0.5s
+}
+
+button:hover {
+  background-position: right center; /* change the direction of the change here */
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+</style>
